2796ffe47b0e4cb5d735a3c3ab0ddd61c849a4e7
sersanor <sersanor@gmail.com>
2014-12-02 01:05:11 +0100

    BT +OK

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,48 +13,62 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-//void MainWindow::startBTService(){
-//// Check if Bluetooth is available on this device
-//if (localDevice.isValid()) {
+void MainWindow::startBTService(){
+    QBluetoothLocalDevice localDevice;
+    QString localDeviceName;
+    // Check if Bluetooth is available on this device
+    if (localDevice.isValid()) {
 
-//    // Turn Bluetooth on
-//    localDevice.powerOn();
+        // Turn Bluetooth on
+        localDevice.powerOn();
+        ui->s_addr->setText(localDevice.address().toString());
+        // Read local device name
+        localDeviceName = localDevice.name();
+        ui->s_name->setText(localDevice.name());
+    }
+}
 
-//    // Read local device name
-//    localDeviceName = localDevice.name();
-
-//    // Make it visible to others
-//    localDevice.setHostMode(QBluetoothLocalDevice::HostDiscoverable);
-
-//    // Get connected devices
-//    QList<QBluetoothAddress> remotes;
-//    remotes = localDevice.connectedDevices();
-//}
-//}
-
-//void MainWindow::startDeviceDiscovery()
-//{
-//    //START BT
-//    startBTService();
-
-//    // Create a discovery agent and connect to its signals
-//    QBluetoothDeviceDiscoveryAgent *discoveryAgent = new QBluetoothDeviceDiscoveryAgent(this);
-//    connect(discoveryAgent, SIGNAL(deviceDiscovered(QBluetoothDeviceInfo)),
-//            this, SLOT(deviceDiscovered(QBluetoothDeviceInfo)));
-
-//    // Start a discovery
-//    discoveryAgent->start();
-
-//    //...
-//}
-
-// In your local slot, read information about the found devices
-//void MainWindow::deviceDiscovered(const QBluetoothDeviceInfo &device)
-//{
-//    qDebug() << "Found new device:" << device.name() << '(' << device.address().toString() << ')';
-//}
 
 void MainWindow::on_startButton_clicked()
 {
-     qDebug() << "BUTTON HANDLER";
+     startBTService();
+
+}
+
+void MainWindow::clientConnected()
+{
+    QBluetoothSocket *socket = rfcommServer->nextPendingConnection();
+    if (!socket)
+        return;
+
+    connect(socket, SIGNAL(readyRead()), this, SLOT(readSocket()));
+    connect(socket, SIGNAL(disconnected()), this, SLOT(clientDisconnected()));
+    clientSockets.append(socket);
+    emit clientConnected(socket->peerName());
+}
+
+void MainWindow::readSocket()
+{
+    QBluetoothSocket *socket = qobject_cast<QBluetoothSocket *>(sender());
+    if (!socket)
+        return;
+
+    while (socket->canReadLine()) {
+        QByteArray line = socket->readLine().trimmed();
+        emit messageReceived(socket->peerName(),
+                             QString::fromUtf8(line.constData(), line.length()));
+    }
+}
+
+void MainWindow::clientDisconnected()
+{
+    QBluetoothSocket *socket = qobject_cast<QBluetoothSocket *>(sender());
+    if (!socket)
+        return;
+
+    emit clientDisconnected(socket->peerName());
+
+    clientSockets.removeOne(socket);
+
+    socket->deleteLater();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,10 @@
 
 #include <QMainWindow>
 #include <QDebug>
+#include <QBluetoothLocalDevice>
+#include <QBluetoothDeviceInfo>
+#include <QBluetoothDeviceDiscoveryAgent>
+#include <QBluetoothServer>
 
 namespace Ui {
 class MainWindow;
@@ -15,15 +19,24 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
-   //void startBTService();
-   //void startDeviceDiscovery();
-   //void deviceDiscovered(const QBluetoothDeviceInfo device);
+   void startBTService();
+
+signals:
+    void messageReceived(const QString &sender, const QString &message);
+    void clientConnected(const QString &name);
+    void clientDisconnected(const QString &name);
 
 private slots:
     void on_startButton_clicked();
+    void clientConnected();
+    void clientDisconnected();
+    void readSocket();
 
 private:
     Ui::MainWindow *ui;
+    QBluetoothServer *rfcommServer;
+       QBluetoothServiceInfo serviceInfo;
+       QList<QBluetoothSocket *> clientSockets;
 };
 
 #endif // MAINWINDOW_H
